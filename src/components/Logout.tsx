1ca7550cffc2229ee77e7ee29bfcf20b0454ecc2
黄宸宇 <2022010789@admin.secoder.net>
2024-03-27 01:32:38 +0800

    fix: use doublequote

--- src/components/Logout.tsx
+++ src/components/Logout.tsx
@@ -1,7 +1,7 @@
 import { RootState } from "../redux/store";
 import { resetAuth } from "../redux/auth";
 import { NextRouter } from "next/router";
-import { Button } from 'antd';
+import { Button } from "antd";
 import { Dispatch } from "redux";
 
 
@@ -9,8 +9,8 @@ const Logout = (router: NextRouter, dispatch: Dispatch, auth: RootState["auth"])
 
     return (
         <>
-            <div style={{ display: 'flex', position: 'fixed', justifyContent: "center", alignItems: "center", top: 10, right: 20, zIndex: 999 }}>
-                <p style={{ color: 'white', marginRight: 10 }}>
+            <div style={{ display: "flex", position: "fixed", justifyContent: "center", alignItems: "center", top: 10, right: 20, zIndex: 999 }}>
+                <p style={{ color: "white", marginRight: 10 }}>
                     欢迎你， 用户{auth.name}!
                 </p>
                 <Button type="primary" danger onClick={() => { dispatch(resetAuth()); router.push("/login") }} >

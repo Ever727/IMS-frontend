726310b3e103b257e42fad320a4413afc72e4a23
lzh <zonghao-22@mails.tsinghua.edu.cn>
2024-03-26 10:20:36 +0800

    fix: fix no-trailing-spaces bugs

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -8,9 +8,9 @@ const App: React.FC = () => {
 
   return (
     <>
-        <Layout>
-        <TextArea allowClear placeholder="Input your message"  />
-        </Layout>
+      <Layout>
+        <TextArea allowClear placeholder="Input your message" />
+      </Layout>
     </>
   );
 };
--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -1,31 +1,31 @@
 import React from "react";
-import { UploadOutlined, UserOutlined, VideoCameraOutlined} from "@ant-design/icons";
-import { Layout, Menu, theme} from "antd";
+import { UploadOutlined, UserOutlined, VideoCameraOutlined } from "@ant-design/icons";
+import { Layout, Menu, theme } from "antd";
 import TextInput from "../components/TextInput";
 import SearchInput from "../components/SearchInput";
 import ScrollList from "../components/ScrollList";
 
-const { Header, Content, Footer, Sider} = Layout;
+const { Header, Content, Footer, Sider } = Layout;
 const items = new Array(10).fill(UserOutlined).map(
   (icon, index) => ({
     key: String(index + 1),
     icon: React.createElement(icon),
     label: `nav ${index + 1}`,
   }),
-); 
+);
 
 const App: React.FC = () => {
   const {
-    token: { colorBgContainer, borderRadiusLG, colorBgLayout, colorSuccessBg},
+    token: { colorBgContainer, borderRadiusLG, colorBgLayout, colorSuccessBg },
   } = theme.useToken();
 
   return (
-    <Layout style={{background: colorBgLayout, height: "800px"}}>
+    <Layout style={{ background: colorBgLayout, height: "800px" }}>
       <Sider width="60px" ></Sider>
       <Sider
         breakpoint="md"
         collapsedWidth="0"
-        style={{background: colorBgLayout}}
+        style={{ background: colorBgLayout }}
         onBreakpoint={(broken) => {
           console.log(broken);
         }}
@@ -33,14 +33,14 @@ const App: React.FC = () => {
           console.log(collapsed, type);
         }}
       >
-        <Layout style={{margin: "0px 0", background: colorBgLayout}}>
-            <SearchInput />
+        <Layout style={{ margin: "0px 0", background: colorBgLayout }}>
+          <SearchInput />
         </Layout>
         <div className="demo-logo-vertical" />
-        <ScrollList/>
+        <ScrollList />
       </Sider>
       <Layout>
-        <Layout style={{ margin: "0", height: "700px"}}>
+        <Layout style={{ margin: "0", height: "700px" }}>
           <div
             style={{
               padding: 24,
@@ -51,8 +51,8 @@ const App: React.FC = () => {
           >
           </div>
         </Layout>
-        <Layout style={{margin: "0"}}>
-            <TextInput />
+        <Layout style={{ margin: "0" }}>
+          <TextInput />
         </Layout>
       </Layout>
     </Layout>

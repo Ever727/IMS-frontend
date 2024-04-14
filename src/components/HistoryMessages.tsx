14eea01fe5d0bc99065f8687cbbbd4ad47ae267f
黄宸宇 <2022010789@admin.secoder.net>
2024-04-14 22:13:10 +0800

    feat: add filter by user

--- src/components/HistoryMessages.tsx
+++ src/components/HistoryMessages.tsx
@@ -3,6 +3,7 @@ import { Avatar, Button, Calendar, Divider, FloatButton, Modal, } from 'antd';
 import { Message } from '../api/types';
 import style from './HistoryMessages.module.css';
 import { Dayjs } from 'dayjs';
+import { CalendarOutlined, CloseOutlined, TeamOutlined } from '@ant-design/icons';
 
 interface ModalProps {
     isOpen: boolean;
@@ -36,7 +37,9 @@ const MSG = (message: Message) => {
 
 const HistoryModal: FC<ModalProps> = ({ isOpen, onCancel, messages }) => {
     const [showCalendar, setShowCalendar] = useState(false);
+    const [showUserSelect, setShowUserSelect] = useState(false);
     const [selectedDate, setSelectedDate] = useState<Dayjs | null>(null);
+    const [selectedUser, setSelectedUser] = useState<string | null>(null);
 
     const availableDates = useMemo(() => {
         if (!messages) return new Set<string>();
@@ -48,14 +51,33 @@ const HistoryModal: FC<ModalProps> = ({ isOpen, onCancel, messages }) => {
         return dates;
     }, [messages]);
 
+    const availableUsers = useMemo(() => {
+        if (!messages) return new Map<string, { id: string; name: string; avatar: string }>();
+
+        const users = new Map<string, { id: string; name: string; avatar: string }>();
+        messages.forEach((msg) => {
+            if (!users.has(msg.senderId)) {
+                users.set(msg.senderId, {
+                    id: msg.senderId,
+                    name: msg.sender,
+                    avatar: msg.avatar,
+                });
+            }
+        });
+        return users;
+    }, [messages]);
+
     const disabledDate = (current: Dayjs) => {
         return !availableDates.has(current.toDate().toDateString());
     };
 
-    const handleCloseCalendar = () => {
+    const handleClose = () => {
         setShowCalendar(false);
         setSelectedDate(null); // 清空选中的日期
+        setShowUserSelect(false);
+        setSelectedUser(null); // 清空选中的用户
     };
+
 
     return (
         <>
@@ -64,7 +86,7 @@ const HistoryModal: FC<ModalProps> = ({ isOpen, onCancel, messages }) => {
                 title="聊天记录"
                 open={isOpen}
                 onCancel={() => {
-                    handleCloseCalendar();
+                    handleClose();
                     onCancel();
                 }}
                 footer={null}
@@ -79,25 +101,52 @@ const HistoryModal: FC<ModalProps> = ({ isOpen, onCancel, messages }) => {
                         disabledDate={disabledDate}
                     />
                 </div>
+
+                <div className={`${style.userSelectOverlay} ${showUserSelect ? style.visible : ''}`}>
+                    <div className={style.memberGrid}>
+                        {availableUsers
+                            ? Array.from(availableUsers.values()).map((item) => (
+                                <div key={item.id} className={style.member} onClick={() => setSelectedUser(item.id)}>
+                                    <Avatar src={item.avatar} alt={item.name} className={style.avatar} />
+                                    <div className={style.name}>{item.name}</div>
+                                </div>
+                            ))
+                            : null}
+                    </div>
+                </div>
+
                 <FloatButton.Group shape="circle" style={{ right: 24 }}>
-                    <FloatButton onClick={() => setShowCalendar((prevState) => !prevState)} />
-                    <FloatButton.BackTop visibilityHeight={0} />
+                    <FloatButton icon={<CalendarOutlined />} onClick={() => setShowCalendar((prevState) => !prevState)} />
+                    <FloatButton icon={<TeamOutlined />} onClick={() => setShowUserSelect((prevState) => !prevState)} />
+                    <FloatButton icon={<CloseOutlined />} onClick={handleClose} />
                 </FloatButton.Group>
 
                 <Divider />
                 <div className={style.container}>
-                    {selectedDate
+                    {selectedDate && selectedUser
                         ? messages
                             .filter((item) => {
                                 return (
-                                    new Date(item.timestamp).toDateString() ===
-                                    selectedDate.toDate().toDateString()
+                                    new Date(item.timestamp).toDateString() === selectedDate.toDate().toDateString() &&
+                                    item.senderId === selectedUser
                                 );
                             })
                             .map((item) => <MSG key={item.id} {...item} />)
-                        : messages ?
-                            messages.map((item) => <MSG key={item.id} {...item} />)
-                            : null}
+                        : selectedDate
+                            ? messages
+                                .filter((item) => {
+                                    return new Date(item.timestamp).toDateString() === selectedDate.toDate().toDateString();
+                                })
+                                .map((item) => <MSG key={item.id} {...item} />)
+                            : selectedUser
+                                ? messages
+                                    .filter((item) => {
+                                        return item.senderId === selectedUser;
+                                    })
+                                    .map((item) => <MSG key={item.id} {...item} />)
+                                : messages
+                                    ? messages.map((item) => <MSG key={item.id} {...item} />)
+                                    : null}
                 </div>
             </Modal>
         </>
